cac44d2c53e7e6c953ae5d14a02270b4a9bc9253
John Angelo Barot <jangelo08@asduff.local>
2017-10-24 16:12:59 +0800

    Added bootstrap-vue in main.js

--- themes/ub/src/main.js
+++ themes/ub/src/main.js
@@ -1,7 +1,16 @@
+
+require('./bootstrap');
+
 import RootComponent from './root';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import { store } from './store';
+import BootstrapVue from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 // set up our global plugins and utilities
 require('./app/boot');
