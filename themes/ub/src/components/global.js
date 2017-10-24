a58e895eb8144b392b2466551a10d4080095cba5
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-10-24 10:20:43 +0800

    organized global vue components

--- themes/ub/src/components/global.js
+++ themes/ub/src/components/global.js
@@ -1,5 +1,33 @@
-// These components are registered globally, and can be used
+// These  are registered globally, and can be used
 // anywhere within our application without importing them.
-export default {
 
+//common components
+import head from '../common/header.vue';
+import foot from '../common/footer.vue';
+
+//components
+import welcome from './welcome';
+import info from './info.vue';
+import contact from './contact.vue';
+import compare from './compare.vue';
+import loanCalculator from './loancalculator.vue';
+import zeroPromo from './zeropromo.vue';
+import promo from './promo.vue';
+import faq from './faq.vue';
+
+
+export default {
+    //common components
+    'heading' : head,
+    'footing' : foot,
+
+    //components
+    'welcome' :  welcome,
+    'info' :  info,
+    'contact' :  contact,
+    'compare' :  compare,
+    'loan-calculator' :  loanCalculator,
+    'zero-promo' :  zeroPromo,
+    'promo' :  promo,
+    'faq' : faq,
 };
--- themes/ub/src/main.js
+++ themes/ub/src/main.js
@@ -3,38 +3,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import { store } from './store';
 
-
-// Common
-import head from './common/header.vue';
-import foot from './common/footer.vue';
-
-// Components import
-import welcome from './components/welcome';
-import info from './components/info.vue';
-import contact from './components/contact.vue';
-import compare from './components/compare.vue';
-import loanCalculator from './components/loancalculator.vue';
-import zeroPromo from './components/zeropromo.vue';
-import promo from './components/promo.vue';
-import faq from './components/faq.vue';
-
 // set up our global plugins and utilities
 require('./app/boot');
-
-//Common components
-Vue.component('heading', head );
-Vue.component('footing', foot);
-
-//Main Components
-Vue.component('welcome', welcome );
-Vue.component('info', info);
-Vue.component('contact', contact );
-Vue.component('compare', compare );
-Vue.component('loan-calculator', loanCalculator );
-Vue.component('zero-promo', zeroPromo );
-Vue.component('promo', promo );
-Vue.component('faq', faq );
-
 
 // set up the router
 import routes from './app/routes';
