e1f10ec4e88459cd08f063ba8da0a7e158e868c8
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-10-25 12:18:56 +0800

    added util.js for checking Empty object

---
+++ themes/ub/src/util.js
@@ -0,0 +1,18 @@
+ module.exports = {
+	// check if each property in the obj is empty
+     checkIfEmpty: (obj) => {
+	  let isEmpty = 0;
+
+	  for(var prop in obj) {
+	    let item = obj[prop];
+
+	    if(_.isNumber(item)) {
+	      isEmpty += item == 0 ? 0 : 1;
+	    } else {
+	      isEmpty += _.isEmpty(item) ? 0 : 1;
+	    }
+	  }
+
+	  return isEmpty != Object.keys(obj).length;
+     },
+ };
