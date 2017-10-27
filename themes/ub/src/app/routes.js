17c0cf6ebc3d77913dbde888acca3dad963b2d28
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2017-10-27 10:53:30 +0800

    registered pages on routes

--- themes/ub/src/app/routes.js
+++ themes/ub/src/app/routes.js
@@ -1,5 +1,11 @@
 import Otp from 'src/pages/public/otp';
 import Home from 'src/pages/public/home';
+import Faq from 'src/pages/public/Faq';
+import PrivacyPolicy from 'src/pages/public/PrivacyPolicy';
+import Terms from 'src/pages/public/TermsAndCondition';
+import Contact from 'src/pages/public/ContactUs';
+
+//Private Pages
 import Private from 'src/pages/private/private';
 import Calculator from 'src/pages/private/calculatorPage';
 import Apply from 'src/pages/private/ApplyPage';
@@ -16,6 +22,27 @@ export default [
         path: '/otp',
         component: Otp,
     },
+    {
+        name: 'Faq',
+        path: '/faq',
+        component: Faq,
+    },
+    {
+        name: 'PrivacyPolicy',
+        path: '/privay-policy',
+        component: PrivacyPolicy,
+    },
+    {
+        name: 'Terms',
+        path: '/terms-and-conditions',
+        component: Terms,
+    },
+    {
+        name: 'Contact',
+        path: '/contact-us',
+        component: Contact,
+    },
+
     {
         name: 'Private',
         path: '/private',
