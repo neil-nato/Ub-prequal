05ddfdfe17160a7a1cf61f5a208b40dc1c47ebc4
John Angelo Barot <jangelo08@asduff.local>
2017-10-27 18:12:34 +0800

    added function for icons

--- themes/ub/src/components/faq.vue
+++ themes/ub/src/components/faq.vue
@@ -3,10 +3,10 @@
         <div class="container">
             <h2>FAQ</h2>
             <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                <div class="panel panel-default" v-for="faqdetail in faqList">
+                <div class="panel panel-default" v-for="(faqdetail, index) in faqList">
                     <div class="faq-container">
-                        <div v-b-toggle="faqdetail.id.toLocaleString()" class="accordion-head" v-on:click="isActive()">
-                          <span id="faq-icon" class="faq-icon"><i :class="icon" aria-hidden="true"></i></span>
+                        <div v-b-toggle="faqdetail.id.toLocaleString()" class="accordion-head" v-on:click="isActive(index)">
+                          <span id="faq-icon" class="faq-icon"><i v-bind:id="index.toLocaleString()" class="fa fa-plus" aria-hidden="true"></i></span>
                           <span class="faq-question">{{ faqdetail.question }}</span>
                         </div>
 
@@ -29,7 +29,6 @@
         data () {
             return {
                 icon: 'fa fa-plus',
-                iconStatus: true,
             };
         },
         computed:{
@@ -42,14 +41,20 @@
             ...mapActions([
                 'loadFaqList',
             ]),
-            isActive() {
-                this.iconStatus = !this.iconStatus;
-                if (this.iconStatus) {
-                    this.icon = 'fa fa-plus';
+            isActive(index) {
+                let el = document.getElementById(index);
+                if (el.classList.contains('fa-plus')) {
+                    el.classList.remove('fa-plus');
+                    el.classList.add('fa-minus');
                 } else {
-                    this.icon = 'fa fa-minus';
-
+                    el.classList.remove('fa-minus');
+                    el.classList.add('fa-plus');
                 }
+                // if (this.iconStatus) {
+                //     this.icon = 'fa fa-plus';
+                // } else {
+                //     this.icon = 'fa fa-minus';
+                // }
             },
         },
 
