71918f91e3c420e66a5c7a0de753b00e48860f14
John Angelo Barot <jangelo08@asduff.local>
2017-10-30 15:02:49 +0800

    updated radio buttons

--- themes/ub/src/components/form/applicantIdentity.vue
+++ themes/ub/src/components/form/applicantIdentity.vue
@@ -44,109 +44,40 @@
 
         <div class="question-item">
             <label>Gender</label>
-            <div class="form-group">
-              <label class="custom-control custom-radio">
-        		  <input  v-model="gender" @change="getApplicant({gender, comaker})"
-              v-validate="'required'" required name="gender" value="0" type="radio" class="custom-control-input">
-        		  <span class="custom-control-indicator"></span>
-        		  <span class="custom-control-description">Male</span>
-        	  </label>
-        		<label class="custom-control custom-radio">
-        		  <input v-model="gender" @change="getApplicant({gender, comaker})"
-              v-validate="'required'" required name="gender" value="1" type="radio" class="custom-control-input">
-        		  <span class="custom-control-indicator"></span>
-        		  <span class="custom-control-description">Female</span></span>
-        	  </label>
+            <b-form-radio-group required id="gender-radio" v-model="gender" name="gender" @input="getApplicant({gender, comaker})">
+              <b-form-radio value="0">Male</b-form-radio>
+              <b-form-radio value="1">Female</b-form-radio>
+            </b-form-radio-group>
             <span v-show="errors.has('gender')" class="help is-danger">{{ error.required }}</span>
-            </div>
         </div>
 
         <div class="question-item">
             <label>Civil Status</label>
-            <div class="form-group">
-                <div class="row">
-                	<div class="form-check col-md-3 col-lg-2">
-                	  <label class="custom-control custom-radio">
-                		  <input  v-model="civilstat" @change="getApplicant({civilstat, comaker})"
-                      v-validate="'required'" required name="civilstat" type="radio" class="custom-control-input" value="single">
-                		  <span class="custom-control-indicator"></span>
-                		  <span class="custom-control-description">Single</span>
-                	  </label>
-                	</div>
-                	<div class="form-check col-md-3 col-lg-2">
-                	  <label class="custom-control custom-radio">
-                		  <input v-model="civilstat" @change="getApplicant({civilstat, comaker})"
-                      v-validate="'required'" required name="civilstat" type="radio" class="custom-control-input" value="divorced">
-                		  <span class="custom-control-indicator"></span>
-                		  <span class="custom-control-description">Divorced</span>
-                	  </label>
-                	</div>
-                	<div class="form-check col-md-3 col-lg-2">
-                	  <label class="custom-control custom-radio">
-                		  <input v-model="civilstat" @change="getApplicant({civilstat, comaker})"
-                      v-validate="'required'" required name="civilstat" type="radio" class="custom-control-input" value="widowed">
-                		  <span class="custom-control-indicator"></span>
-                		  <span class="custom-control-description">Widowed</span>
-                	  </label>
-                	</div>
-                	<div class="form-check col-md-3 col-lg-2">
-                	  <label class="custom-control custom-radio">
-                		  <input v-model="civilstat" @change="getApplicant({civilstat, comaker})"
-                      v-validate="'required'" required name="civilstat" type="radio" class="custom-control-input" value="separated">
-                		  <span class="custom-control-indicator"></span>
-                		  <span class="custom-control-description">Seperated</span>
-                	  </label>
-                	</div>
-                </div>
-                <div class="row">
-                	<div class="form-check col-md-3 col-lg-2">
-                	  <label class="custom-control custom-radio">
-                		  <input v-model="civilstat" @change="getApplicant({civilstat, comaker})"
-                      v-validate="'required'" required name="civilstat" type="radio" class="custom-control-input" value="married">
-                		  <span class="custom-control-indicator"></span>
-                		  <span class="custom-control-description">Married</span>
-                	  </label>
-                	</div>
-                	<div class="form-check col-md-3 col-lg-2">
-                	  <label class="custom-control custom-radio">
-                		  <input v-model="civilstat" @change="getApplicant({civilstat, comaker})"
-                      v-validate="'required'" name="civilstat" type="radio" class="custom-control-input" value="anulled">
-                		  <span class="custom-control-indicator"></span>
-                		  <span class="custom-control-description">Anulled</span>
-                	  </label>
-                	</div>
-                	<div class="form-check col-md-3 col-lg-2">
-                	 <label class="custom-control custom-radio">
-                		  <input v-model="civilstat" @change="getApplicant({civilstat, comaker})"
-                      v-validate="'required'"  required name="civilstat" type="radio" class="custom-control-input" value="live-in">
-                		  <span class="custom-control-indicator"></span>
-                		  <span class="custom-control-description">Live-in</span>
-                	  </label>
-                	</div>
-                </div>
-
-                <span v-show="errors.has('civilstat')" class="help is-danger">{{ error.required }}</span>
-            </div>
+            <b-form-radio-group required id="civilstat-radio" v-model="civilstat" name="civilstat" @input="getApplicant({civilstat, comaker})">
+              <b-form-radio value="single">Single</b-form-radio>
+              <b-form-radio value="divorced">Divorced</b-form-radio>
+              <b-form-radio value="widowed">Widowed</b-form-radio>
+              <b-form-radio value="separated">Separated</b-form-radio>
+              <b-form-radio value="married">Married</b-form-radio>
+              <b-form-radio value="Anulled">Anulled</b-form-radio>
+              <b-form-radio value="live-in">Live-in</b-form-radio>
+            </b-form-radio-group>
+            <span v-show="errors.has('civilstat')" class="help is-danger">{{ error.required }}</span>
         </div>
 
         <div class="question-item">
             <label>Citizenship</label>
-            <div class="form-group form-inline">
-              <label class="custom-control custom-radio">
-                  <input v-model="nationality" @change="getApplicant({nationality, comaker})"
-                  v-validate="'required'" required name="nationality" value="FIL" type="radio" class="custom-control-input">
-                  <span class="custom-control-indicator"></span>
-                  <span class="custom-control-description">Filipino</span>
-              </label>
-                <label class="custom-control custom-radio">
-                  <input v-model="nationality" @change="getApplicant({nationality, comaker})"
-                  v-validate="'required'"  required name="nationality" value="OTH" type="radio" class="custom-control-input">
-                  <span class="custom-control-indicator"></span>
-                  <span class="custom-control-description">Other</span>
-              </label>
-                <input type="text" name="acrno" class="form-control form-others mb-2 mr-sm-2 mb-sm-0 col-md-6" placeholder="ACR Number">
-            </div>
+            <b-form-radio-group required id="civilstat-radio" v-model="nationality" name="nationality" @input="getApplicant({nationality, comaker})">
+              <b-form-radio value="FIL">Filipino</b-form-radio>
+              <b-form-radio value="OTH">Other</b-form-radio>
+            </b-form-radio-group>
             <span v-show="errors.has('nationality')" class="help is-danger">{{ error.required }}</span>
+        </div>
+
+        <div class="question-item">
+          <label>ACR No.</label>
+          <input type="text" v-model="acrno" @input="getApplicant({acrno, comaker})"
+          name="acrno" class="form-control form-others mb-2 mr-sm-2 mb-sm-0 col-md-6" placeholder="ACR Number"/>
         </div>
 
         <div class="question-item">
@@ -214,12 +145,12 @@
             </div>
 
             <div class="form-group row">
-                <label class="col-md-2 col-form-label">Years of Stay:</label>
+                <label class="col-md-2 col-form-label years-of-stay">Years of Stay:</label>
 
                 <div class="my-2 col-md-4 col-lg-3">
                     <label class="sr-only" for="lengthYears">Years</label>
                     <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-                        <input v-model="lengthofstay_y" @input="getApplicant({lengthofstay_y, comaker})" type="number" class="form-control" id="lengthYears" name="lengthofstay_y" placeholder="No. of Years">
+                        <input v-model="lengthofstay_y" @input="getApplicant({lengthofstay_y, comaker})" min="0" type="number" class="form-control" id="lengthYears" name="lengthofstay_y" placeholder="No. of Years">
                         <div class="input-group-addon">Yrs.</div>
                     </div>
                 </div>
@@ -227,7 +158,7 @@
                 <div class="my-2 col-md-4 col-lg-3">
                     <label class="sr-only" for="lengthYears">Months</label>
                     <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-                        <input v-model="lengthofstay_m" @input="getApplicant({lengthofstay_m, comaker})"  type="number" class="form-control" id="lengthYears" name="lengthofstay_m" placeholder="No. of Months">
+                        <input v-model="lengthofstay_m" @input="getApplicant({lengthofstay_m, comaker})" min="0"  type="number" class="form-control" id="lengthYears" name="lengthofstay_m" placeholder="No. of Months">
                         <div class="input-group-addon">Mos.</div>
                     </div>
                 </div>
